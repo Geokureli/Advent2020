bd509c2a6e8deca3d33874290d8f50b96d78c756
George FunBook <gkurelic@gmail.com>
2020-12-03 20:07:15 -0600

    remove splash

--- source/Main.hx
+++ source/Main.hx
@@ -15,8 +15,8 @@ class Main extends openfl.display.Sprite
 	public function new()
 	{
 		super();
-		// addChild(new flixel.FlxGame(240, 135, states.BootState));
-		addChild(new flixel.FlxGame(480, 270, states.BootState));
+		// addChild(new flixel.FlxGame(240, 135, states.BootState, 1, 60, 60, true));
+		addChild(new flixel.FlxGame(480, 270, states.BootState, 1, 60, 60, true));
 		// addChild(new flixel.FlxGame(960, 540, states.BootState));
 	}
 }
