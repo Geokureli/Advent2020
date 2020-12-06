4da8db9a2f95d68d17818172d162700154e3fdc1
George FunBook <gkurelic@gmail.com>
2020-12-05 23:29:07 -0600

    outside stuff

--- source/states/rooms/OutsideState.hx
+++ source/states/rooms/OutsideState.hx
@@ -14,9 +14,11 @@ class OutsideState extends RoomState
         super.initEntities();
         
         var shine = background.setAnimFrameRate("shine", 4);
-        background.getByName("stars").scrollFactor.y = 0.5;
-        background.getByName("moon").scrollFactor.y = 0.5;
+        background.getByName("stars").scrollFactor.y = 0.15;
+        background.getByName("moon").scrollFactor.y = 0.35;
+        background.getByName("mountains").scrollFactor.y = 0.5;
         background.getByName("lake").scrollFactor.y = 0.75;
+        background.getByName("ice_1").scrollFactor.y = 0.75;
         background.getByName("shine").scrollFactor.y = 0.75;
     }
     
