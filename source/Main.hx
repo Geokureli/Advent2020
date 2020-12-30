059972ab691bb56ff74dc0b3d806d23d4ff2ca75
George FunBook <gkurelic@gmail.com>
2020-12-30 16:35:51 -0600

    revert merge fuckups

--- source/Main.hx
+++ source/Main.hx
@@ -10,5 +10,8 @@ class Main extends openfl.display.Sprite
 		// addChild(new flixel.FlxGame(240, 135, states.BootState, 1, 60, 60, true));
 		addChild(new flixel.FlxGame(480, 270, states.BootState, 1, 60, 60, true));
 		// addChild(new flixel.FlxGame(960, 540, states.BootState));
+		
+		trace("version:" + openfl.Lib.application.meta.get("version"));
+		trace("render context: " + stage.window.context.type);
 	}
 }
