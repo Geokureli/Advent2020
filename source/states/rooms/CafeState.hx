e0d4bee4392310363a52b34f7f0cb4183be481cd
George FunBook <gkurelic@gmail.com>
2021-12-09 18:46:11 -0600

    add jukebox

--- source/states/rooms/CafeState.hx
+++ source/states/rooms/CafeState.hx
@@ -5,15 +5,14 @@ class CafeState extends RoomState
     override function create()
     {
         super.create();
+        
+        #if LOAD_DISK_CAROUSEL
+        var juke = foreground.assertByName("cafe-juke");
+        addHoverTextTo(juke, "Music", ()->openSubState(new MusicSelectionSubstate()));
+        #end
     }
     
-    override function initEntities()
-    {
-        super.initEntities();
-    }
-    
-    override function update(elapsed:Float)
-    {
-        super.update(elapsed);
-    }
+    // override function initEntities() { super.initEntities(); }
+
+    // override function update(elapsed:Float) { super.update(elapsed); }
 }
