c5e4998c939015a62026c272fca27849660193c3
Brandon <brandy@scarecrowarts.com>
2021-12-13 22:51:40 -0500

    mindchamber exists!

--- source/states/rooms/PostOfficeState.hx
+++ source/states/rooms/PostOfficeState.hx
@@ -1,13 +1,47 @@
 package states.rooms;
 
+import states.OgmoState;
+import flixel.FlxSprite;
+
+import flixel.text.FlxBitmapText;
+
 class PostOfficeState extends RoomState
 {
+    var mail:FlxSprite;
+    var nameText = new FlxBitmapText();
+
     override function create()
     {
+        entityTypes["MaleMailMan"] = cast initMaleMailMan;
+        
         super.create();
     }
+
+    function initMaleMailMan(data:OgmoEntityData<Dynamic>)
+    {
+        mail = OgmoEntityData.createFlxSprite(data);
+        mail.loadGraphic("assets/images/player/mindchamber.png");
+        mail.scale.scale(2);
+        
+        return mail;
+    }
     
-    // override function initEntities() { super.initEntities(); }
+    override function initEntities() { 
+        super.initEntities();
+
+        foreground.add(mail);
+
+        nameText.text = "MindChamber";
+        nameText.alignment = CENTER;
+        nameText.color = 0xFFffffff;
+        nameText.borderColor = 0xFF000000;
+        nameText.borderStyle = SHADOW;
+
+        nameText.x = mail.x + (mail.width - nameText.width) / 2;
+        nameText.y = mail.y + mail.height - mail.frameHeight - nameText.height - 4;
+
+        topGround.add(nameText);
+    }
 
     // override function update(elapsed:Float) { super.update(elapsed); }
 }
